f187747c74899e3a4a5ff3fe5fd649f96f81fa14
Brandyy2 <b.wingfield4591@gmail.com>
2023-11-21 17:06:07 -0500

    login menu; not finished but the display

--- src/main/java/com/example/perfectpaws/HelloController.java
+++ src/main/java/com/example/perfectpaws/HelloController.java
@@ -1,14 +1,5 @@
 package com.example.perfectpaws;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
