76e1426dd1a9d61698380881f11d5e3560e18ea5
Brandyy2 <b.wingfield4591@gmail.com>
2023-11-21 19:32:22 -0500

    login menu; not finished but the display

--- src/main/java/com/example/perfectpaws/HelloController.java
+++ src/main/java/com/example/perfectpaws/HelloController.java
@@ -1,5 +1,33 @@
 package com.example.perfectpaws;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class HelloController {
+
+    @FXML
+    private PasswordField passwordHidden;
+
+    @FXML
+    private TextField passwordVisable;
+
+    @FXML
+    private CheckBox checkBox;
+    @FXML
+    void showPassword(ActionEvent event) {
+        if (checkBox.isSelected()){
+            passwordVisable.setText(passwordHidden.getText());
+            passwordVisable.setVisible(true);
+            passwordHidden.setVisible(false);
+            return;
+        }
+        passwordHidden.setText(passwordVisable.getText());
+        passwordHidden.setVisible(true);
+        passwordVisable.setVisible(false);
+
+    }
 
 }
