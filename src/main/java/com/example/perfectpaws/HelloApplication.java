15d64337f0fb9cb4912faa5c904b40352b4e6470
Brandyy2 <b.wingfield4591@gmail.com>
2023-11-23 11:48:42 -0500

    changed prompt name

--- src/main/java/com/example/perfectpaws/HelloApplication.java
+++ src/main/java/com/example/perfectpaws/HelloApplication.java
@@ -8,11 +8,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 625,550 );
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 635,550 );
+        stage.setTitle("Login Page");
         stage.setScene(scene);
         stage.show();
     }
